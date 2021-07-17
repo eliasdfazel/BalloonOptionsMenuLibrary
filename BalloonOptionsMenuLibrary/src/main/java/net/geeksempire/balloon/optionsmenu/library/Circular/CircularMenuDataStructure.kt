3295f6a32900996838d205b6165492cfd5fa9d1c
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-07-17 04:06:24 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- BalloonOptionsMenuLibrary/src/main/java/net/geeksempire/balloon/optionsmenu/library/Circular/CircularMenuDataStructure.kt
+++
@@ -1,5 +0,0 @@
-package net.geeksempire.balloon.optionsmenu.library.Circular
-
-import android.graphics.drawable.Drawable
-
-data class CircularMenuDataStructure (var itemIcon: Drawable?, var itemDescription: String? = null)
--- BalloonOptionsMenuSample/src/main/java/net/geeksempire/balloon/optionsmenu/BalloonOptionsMenuPhone.kt
+++ BalloonOptionsMenuSample/src/main/java/net/geeksempire/balloon/optionsmenu/BalloonOptionsMenuPhone.kt
@@ -1,29 +1,59 @@
 package net.geeksempire.balloon.optionsmenu
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import net.geeksempire.balloon.optionsmenu.databinding.BalloonOptionsMenuLayoutBinding
+import net.geeksempire.balloon.optionsmenu.databinding.BalloonOptionsMenuSampleLayoutBinding
 import net.geeksempire.balloon.optionsmenu.library.BalloonItemsAction
 import net.geeksempire.balloon.optionsmenu.library.BalloonOptionsMenu
-import net.geeksempire.balloon.optionsmenu.library.Circular.CircularMenuDataStructure
-import net.geeksempire.balloon.optionsmenu.library.Circular.CircularOptionMenu
+import net.geeksempire.balloon.optionsmenu.library.Circular.CircleMenuView
+import net.geeksempire.balloon.optionsmenu.library.Utils.dpToPixel
 
 class BalloonOptionsMenuPhone : AppCompatActivity() {
 
-    lateinit var balloonOptionsMenuLayoutBinding: BalloonOptionsMenuLayoutBinding
+    lateinit var balloonOptionsMenuLayoutBinding: BalloonOptionsMenuSampleLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        balloonOptionsMenuLayoutBinding = BalloonOptionsMenuLayoutBinding.inflate(layoutInflater)
+        balloonOptionsMenuLayoutBinding = BalloonOptionsMenuSampleLayoutBinding.inflate(layoutInflater)
         setContentView(balloonOptionsMenuLayoutBinding.root)
 
-        CircularOptionMenu(context = this@BalloonOptionsMenuPhone,
-            rootView = balloonOptionsMenuLayoutBinding.rootView,
-            optionContainerView = balloonOptionsMenuLayoutBinding.circularOptionContainer,
-            arrayListOf(CircularMenuDataStructure(getDrawable(android.R.drawable.sym_contact_card)))
-        )
+        val circularOptionMenu = CircleMenuView(this@BalloonOptionsMenuPhone,
+            arrayListOf(getDrawable(R.drawable.message_icon), getDrawable(R.drawable.message_icon), getDrawable(R.drawable.message_icon), getDrawable(R.drawable.message_icon), getDrawable(R.drawable.message_icon), getDrawable(R.drawable.message_icon)),
+            arrayListOf(Color.GREEN, Color.BLUE, Color.MAGENTA, Color.GREEN, Color.BLUE, Color.MAGENTA))
+
+        circularOptionMenu.distance = dpToPixel(applicationContext, 51f)
+
+        balloonOptionsMenuLayoutBinding.circularOptionContainer.addView(circularOptionMenu)
+
+        circularOptionMenu.setEventListener(object : CircleMenuView.EventListener() {
+            override fun onMenuOpenAnimationStart(view: CircleMenuView) {
+                Log.d("D", "onMenuOpenAnimationStart")
+            }
+
+            override fun onMenuOpenAnimationEnd(view: CircleMenuView) {
+                Log.d("D", "onMenuOpenAnimationEnd")
+            }
+
+            override fun onMenuCloseAnimationStart(view: CircleMenuView) {
+                Log.d("D", "onMenuCloseAnimationStart")
+            }
+
+            override fun onMenuCloseAnimationEnd(view: CircleMenuView) {
+                Log.d("D", "onMenuCloseAnimationEnd")
+            }
+
+            override fun onButtonClickAnimationStart(view: CircleMenuView, index: Int) {
+                Log.d("D", "onButtonClickAnimationStart| index: $index")
+            }
+
+            override fun onButtonClickAnimationEnd(view: CircleMenuView, index: Int) {
+                Log.d("D", "onButtonClickAnimationEnd| index: $index")
+            }
+        })
+
 
         balloonOptionsMenuLayoutBinding.balloonOptionsMenuTest.setOnClickListener { anchorView ->
 
